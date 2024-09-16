c3c2b4a3fb69132087dd55703ecc342f85a2227c
rajsinghthakure <rajthakur8827142011@gmail.com>
2024-09-16 10:50:50 +0530

    first commit

---
+++ ICMS2/Models/Mapping.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICMS2.Models
+{
+    public class Mapping
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductId { get; set; }
+        public int UserPrice { get; set; }
+        public string UserItemCode { get; set; }
+    }
+}
---
+++ ICMS2/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ICMS2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICMS2.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Mapping> Mappings { get; set; }
+    }
+}
